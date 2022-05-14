bcc3d437acd5a56733f04e1a75f557c24a03dddc
louis <luchongkai96@gmail.com>
2022-05-14 13:32:59 +0800

    ff

--- configs/mvit/mvit_spatialattn_16x4_kinetics400_video.py
+++ configs/mvit/mvit_spatialattn_16x4_kinetics400_video.py
@@ -5,7 +5,7 @@ _base_ = ["../base/schedules/adamw_3e-4_30.py",
 # model settings
 model = dict(
     type='Recognizer3D',
-    backbone=dict(type='MViT', arch='base_16x4', pretrained=False),
+    backbone=dict(type='MViTMe', arch='base_16x4', pretrained=False),
     cls_head=dict(type='MViTHead', num_classes=400, in_channels=768),
     train_cfg=None,
     test_cfg=dict(average_clips='prob'))
@@ -16,4 +16,4 @@ log_config = dict(interval=1000)
 # lr settings
 data = dict(videos_per_gpu=4)
 # work_dir
-work_dir = './work_dirs/mvit_16x4_kinetics400_video/'
+work_dir = './work_dirs/mvit_me_16x4_kinetics400_video/'
