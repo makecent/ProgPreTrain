ef106c7aa2b3a197ab71092f9fc8dc29ec8c2ef3
louis <luchongkai96@gmail.com>
2022-05-19 23:40:52 +0800

    ff

--- configs/mvit/mvit-s_16x4_kinetics400_video.py
+++ configs/mvit/mvit-s_16x4_kinetics400_video.py
@@ -1,13 +1,14 @@
-_base_ = ["../base/schedules/adamw_3e-4_30.py",
+_base_ = ["adamw_3e-4_30.py",
           "default_runtime.py",
-          "../base/datasets/kinetics400/video/kinetics400_16x4x1x224x224_video.py"]
+          "kinetics400_16x4x1x224x224_video.py"]
 
 # model settings
 model = dict(
     type='Recognizer3D',
     backbone=dict(type='MViT', arch='small_16x4', pretrained=False),
     cls_head=dict(type='MViTHead', num_classes=400, in_channels=512),
-    train_cfg=dict(blending=dict(type='MixupBlending', num_classes=400, alpha=.8)),
+    # train_cfg=dict(blending=dict(type='MixupBlending', num_classes=400, alpha=.8)),
+    train_cfg=None,
     test_cfg=dict(average_clips='prob'))
 
 optimizer = dict(lr=3e-4)
--- configs/mvit/mvit-s_2d_16x4_kinetics400_video.py
+++ configs/mvit/mvit-s_2d_16x4_kinetics400_video.py
@@ -1,16 +1,17 @@
-_base_ = ["../base/schedules/adamw_3e-4_30.py",
+_base_ = ["adamw_3e-4_30.py",
           "default_runtime.py",
-          "../base/datasets/kinetics400/video/kinetics400_16x4x1x224x224_video.py"]
+          "kinetics400_16x4x1x224x224_video.py"]
 
 # model settings
 model = dict(
     type='Recognizer3D',
     backbone=dict(type='MViT2D', arch='small_16x4', pretrained=False),
     cls_head=dict(type='MViTHead', num_classes=400, in_channels=512),
-    train_cfg=dict(blending=dict(type='MixupBlending', num_classes=400, alpha=.8)),
+    # train_cfg=dict(blending=dict(type='MixupBlending', num_classes=400, alpha=.8)),
+    train_cfg=None,
     test_cfg=dict(average_clips='prob'))
 
-optimizer = dict(lr=1e-2)
+optimizer = dict(lr=3e-4)
 log_config = dict(interval=1000)
 # lr settings
 data = dict(videos_per_gpu=8)
