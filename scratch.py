e75ae8112c492ab94dd0126232db886c049ef635
louis-4 <luchongkai96@gmail.com>
2022-05-26 17:17:51 +0800

    add some configs

--- scratch.py
+++ scratch.py
@@ -1,10 +1,11 @@
+# %% fvcore
 import torch
 from fvcore.nn import FlopCountAnalysis, parameter_count, flop_count_table
 from mmcv import Config
 from mmaction.models.builder import build_model, build_backbone
 
-inputs = torch.randn(1, 3, 16, 224, 224)
-model = build_backbone(Config.fromfile("configs/mvit/mvit-s_2d_16x4_kinetics400_video.py").model.backbone)
+inputs = torch.randn(1, 3, 8, 224, 224)
+model = build_backbone(Config.fromfile("configs/timesformer/timesformer_jointST_8x32x1_15e_kinetics400_rgb.py").model.backbone)
 
 # inputs = (torch.randn(1, 1, 3, 16, 224, 224), torch.randint(10, (1, 1)))
 # model = build_model(Config.fromfile("configs/mvit/mvit_16x4_kinetics400_video.py").model)
