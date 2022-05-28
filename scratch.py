f1dea0f6fdde00d9ed763ad2f0ea678a7b71b110
louis-4 <luchongkai96@gmail.com>
2022-05-28 20:06:09 +0800

    ff

--- scratch.py
+++ scratch.py
@@ -5,7 +5,8 @@ from mmcv import Config
 from mmaction.models.builder import build_model, build_backbone
 
 inputs = torch.randn(1, 3, 8, 224, 224)
-model = build_backbone(Config.fromfile("configs/timesformer/timesformer_divS_8x4x1_15e_sthv2_rgb.py").model.backbone)
+model = build_backbone(Config.fromfile(
+    "configs/timesformer/timesformer_jointST_extdim_8x4x1_15e_sthv2_rgb.py").model.backbone)
 
 # inputs = (torch.randn(1, 1, 3, 16, 224, 224), torch.randint(10, (1, 1)))
 # model = build_model(Config.fromfile("configs/mvit/mvit_16x4_kinetics400_video.py").model)
