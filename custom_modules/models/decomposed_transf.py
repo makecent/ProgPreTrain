f3fc4365ed49e9d2fb45cb3b8489d29319475f33
louis-4 <luchongkai96@gmail.com>
2022-06-24 12:41:48 +0800

    ff

--- custom_modules/models/decomposed_transf.py
+++ custom_modules/models/decomposed_transf.py
@@ -234,20 +234,9 @@ class TimeSpaceAttention(nn.Module):
         self._reset_parameters()
 
     def _reset_parameters(self):
-        if self._qkv_same_embed_dim:
-            xavier_uniform_(self.in_proj_weight)
-        else:
-            xavier_uniform_(self.q_proj_weight)
-            xavier_uniform_(self.k_proj_weight)
-            xavier_uniform_(self.v_proj_weight)
-
-        if self.in_proj_bias is not None:
-            constant_(self.in_proj_bias, 0.)
-            constant_(self.out_proj.bias, 0.)
-        if self.bias_k is not None:
-            xavier_normal_(self.bias_k)
-        if self.bias_v is not None:
-            xavier_normal_(self.bias_v)
+        xavier_uniform_(self.in_proj_weight)
+        constant_(self.in_proj_bias, 0.)
+        constant_(self.out_proj.bias, 0.)
 
     def forward(self, query):
         # cls_token = query[:, 0, :].unsqueeze(1)
