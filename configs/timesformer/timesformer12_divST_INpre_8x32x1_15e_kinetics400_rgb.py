137546900efd5a7ba153def4b64ac48620183655
louis-4 <luchongkai96@gmail.com>
2022-07-07 19:39:40 +0800

    clean up the configs

--- configs/timesformer/timesformer12_divST_INpre_8x32x1_15e_kinetics400_rgb.py
+++ configs/timesformer/timesformer12_divST_INpre_8x32x1_15e_kinetics400_rgb.py
@@ -1,4 +1,3 @@
-_base_ = ['default_runtime.py']
 
 # model settings
 model = dict(
@@ -7,7 +6,7 @@ model = dict(
         type='TimeSformer',
         pretrained=  # noqa: E251
         'https://download.openmmlab.com/mmaction/recognition/timesformer/vit_base_patch16_224.pth',  # noqa: E501
-        num_transformer_layers=6,
+        num_transformer_layers=12,
         num_frames=8,
         img_size=224,
         patch_size=16,
@@ -15,7 +14,7 @@ model = dict(
         in_channels=3,
         dropout_ratio=0.,
         transformer_layers=None,
-        attention_type='divST_convT',
+        attention_type='divided_space_time',
         norm_cfg=dict(type='LN', eps=1e-6)),
     cls_head=dict(type='TimeSformerHead', num_classes=400, in_channels=768),
     # model training and testing settings
@@ -112,7 +111,7 @@ optimizer = dict(
             '.backbone.time_embed': dict(decay_mult=0.0)
         }),
     weight_decay=1e-4,
-    nesterov=True)  # 0.005 for batch size 64
+    nesterov=True)  # this lr is used for 8 gpus
 optimizer_config = dict(grad_clip=dict(max_norm=40, norm_type=2))
 
 # learning policy
